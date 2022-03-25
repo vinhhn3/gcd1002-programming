47024d3c456b24fbd19e7fdbad147cf5293b4107
Vinh Hoang <vinhhn3@fe.edu.vn>
2022-03-25 15:31:10 +0700

    Add project files.

---
+++ BasicIntroduction/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace BasicIntroduction
+{
+  internal class Program
+  {
+    static void SolveFirstDegreeFunction(double a, double b)
+    {
+      if (a != 0)
+      {
+        Console.WriteLine($"Solution of x: {-b / a}");
+      }
+      else if (b != 0)
+      {
+        Console.WriteLine("No Solution of x");
+      }
+      else
+      {
+        Console.WriteLine("Indefinite solution number of x");
+      }
+    }
+
+    static void Main(string[] args)
+    {
+      Console.WriteLine("Hello World!");
+      SolveFirstDegreeFunction(1, 2);
+      SolveFirstDegreeFunction(0, 2);
+      SolveFirstDegreeFunction(1, 0);
+
+    }
+  }
+
+
+}
---
+++ BasicSyntax/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace BasicSyntax
+{
+  internal class Program
+  {
+    static void Main(string[] args)
+    {
+      // Declare Variable
+      // DataType variableName = value;
+      // Variable must be meaningful
+      int myAge = 10;
+      double mySalaryAtGreenwich = 10.0;
+      float myHeight = 123;
+      string fullName = "Hoang Nhu Vinh";
+      char myFavorite = 'c';
+
+      // Print to screen
+      Console.WriteLine(myAge);
+      Console.WriteLine("My Salary: " + mySalaryAtGreenwich);
+      Console.WriteLine($"My Full Name is: {fullName}"); // My Full Name is Hoang Nhu Vinh
+      Console.WriteLine($"My height: {myHeight} and my favorite is: {myFavorite}");
+      Console.WriteLine("My height is: {0} and my favorite is: {1}",
+          myHeight, myFavorite
+        );
+
+      // Receive Input
+      Console.WriteLine("Enter your address: ");
+      string address = Console.ReadLine(); // return string
+      Console.WriteLine($"Address is: {address}");
+
+      // Convert string to int
+      Console.WriteLine("Enter your new age: ");
+      myAge = int.Parse(Console.ReadLine());
+      Console.WriteLine($"Your new age: {myAge}");
+    }
+  }
+}
