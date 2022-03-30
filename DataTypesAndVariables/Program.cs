e9f11097fd1c30cf4af472fc69818634ab8e2c2b
Vinh Hoang <vinhhn3@fe.edu.vn>
2022-03-30 14:50:07 +0700

    Type conversion

--- DataTypesAndVariables/Program.cs
+++ DataTypesAndVariables/Program.cs
@@ -35,6 +35,22 @@ namespace DataTypesAndVariables
       Console.WriteLine((0.1 + 0.2) - 0.3 == 0);  // false
       float mySalary = 1.1234567F;    // 7 digits => 32 bits
       double pi = 3.141231241111231D; // 15 - 16 digits => 64 bits
+
+      // Type Conversion
+      float size = 70.12f;        // 32 bits
+      // Direct Conversion
+      double sizeInDouble = size; // 64 bits
+
+      double weight = 70.12;        // 64 bits
+      //float weightInFloat = weight;  Cannot convert because 32 bits (float) < 64 bits (double)
+
+      // Explicit Conversion
+      float weightInFloat = (float)weight;
+
+      double age = 18.15;       // 64 bits
+      int ageInt = (int)age;    // 32 bits
+
+      Console.WriteLine(ageInt);  // 18
     }
   }
 }
