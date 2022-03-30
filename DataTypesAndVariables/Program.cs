2d90d39cdffba12d6056a49097be4272b39d3818
Vinh Hoang <vinhhn3@fe.edu.vn>
2022-03-30 14:59:51 +0700

    Exercise: Convert Centuries to Minutes

--- DataTypesAndVariables/Program.cs
+++ DataTypesAndVariables/Program.cs
@@ -51,6 +51,28 @@ namespace DataTypesAndVariables
       int ageInt = (int)age;    // 32 bits
 
       Console.WriteLine(ageInt);  // 18
+
+      // Exercise
+      /*
+      Write program to enter an integer number of centuries 
+      and convert it to years, days, hours and minutes
+      1 centuries = 100 years = 36524 days = 876576 hours = 52594560 minutes
+      5 centuries = 500 years = 182621 days = 4382904 hours = 262974240 minutes
+      */
+
+      Console.WriteLine("Enter centuries:");
+      int centuries = int.Parse(Console.ReadLine());
+      int years = centuries * 100;
+      int days = (int)(years * 365.2422);
+      int hours = days * 24;
+      int minutes = hours * 60;
+
+      Console.WriteLine(
+        $"{centuries} centuries = " +
+        $"{years} years = " +
+        $"{days} days = " +
+        $"{hours} hours = " +
+        $"{minutes} minutes");
     }
   }
 }
