16375e6fd099800acf6a6221879ecebbac47cb66
Vinh Hoang <vinhhn3@fe.edu.vn>
2022-04-06 14:06:47 +0700

    Stack Vs. Heap

--- Methods/Program.cs
+++ Methods/Program.cs
@@ -14,6 +14,31 @@ namespace Methods
       int humanCost = 120;
       int materialCost = 140;
       int totalCost = SumTwoInt(humanCost, materialCost);
+
+      int age = 10;               // Stack
+      IncreaseAge(age);         // Heap
+
+      Console.WriteLine(age); // 10 Stack
+      int[] arrayAge = new int[]
+      {
+        10
+      };
+
+      Console.WriteLine(arrayAge[0]);   // 10 Heap
+      IncreaseAgeArray(arrayAge);        // Heap
+      Console.WriteLine(arrayAge[0]);   // 11 
+
+    }
+
+    static void IncreaseAgeArray(int[] arr)
+    {
+      arr[0] = arr[0] + 1;
+    }
+
+    static void IncreaseAge(int a)
+    {
+      a = a + 1;
+      Console.WriteLine(a); // 11 Heap
     }
 
     // Define a method/function:
