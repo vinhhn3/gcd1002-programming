b5645d69e41ecba72aa9a705b57182e7720219a8
Vinh Hoang <64019402+vinhhn3@users.noreply.github.com>
2022-04-06 15:52:37 +0700

    4 methods session (#18)
    * Method Introduction
    
    * Stack Vs. Heap
    
    * Overloading methods
    
    * Stackoverflow
    
    * Problem: Multiply Evens by Odds
    
    * Refactor

---
+++ Methods/Program.cs
@@ -0,0 +1,93 @@
+﻿using System;
+
+namespace Methods
+{
+  internal class Program
+  {
+    static void Main(string[] args)
+    {
+      Hello();
+      Console.WriteLine("Hello World!");
+      int fixedSalary = 100;
+      int bonus = 20;
+      int totalSalary = SumTwoInt(fixedSalary, bonus); // 120
+
+      int humanCost = 120;
+      int materialCost = 140;
+      int totalCost = SumTwoInt(humanCost, materialCost);
+
+      int age = 10;               // Stack
+      IncreaseAge(age);         // Heap
+
+      Console.WriteLine(age); // 10 Stack
+      int[] arrayAge = new int[]
+      {
+        10
+      };
+
+      Console.WriteLine(arrayAge[0]);   // 10 Heap
+      IncreaseAgeArray(arrayAge);        // Heap
+      Console.WriteLine(arrayAge[0]);   // 11 
+
+      string name = "Vinh Hoang";
+      PrintHello(name);
+      PrintHello();
+
+    }
+
+    static void IncreaseAgeArray(int[] arr)
+    {
+      arr[0] = arr[0] + 1;
+    }
+
+    static void IncreaseAge(int a)
+    {
+      a = a + 1;
+      Console.WriteLine(a); // 11 Heap
+    }
+
+    // Define a method/function:
+    // DataTypeReturn MethodName(dataType arg1, dataType arg2){
+    //  doSomething();
+    // }
+    static int SumTwoInt(int a, int b)
+    {
+      int result = a + b;
+      return result;
+    }
+
+    static int SumThreeInt(int a, int b, int c)
+    {
+      int result = SumTwoInt(a, SumTwoInt(b, c));
+      return result;
+    }
+
+    // Method without Parameters
+    static string PrintHello()
+    {
+      return "This is default Hello World !!!";
+    }
+
+    // Method without return data type (void)
+    static void PrintHello(string name)
+    {
+      Console.WriteLine($"Hello {name}. Welcome to CSharp");
+    }
+
+    static string PrintHello(string firstName, string lastName)
+    {
+      return firstName + " " + lastName;
+    }
+
+    static void Hello()
+    {
+      Bonjour();
+    }
+
+    static void Bonjour()
+    {
+      Hello();
+    }
+
+  }
+}
