488fe12d3635ec16591dd689fc995f0cbff919bd
Vinh Hoang <vinhhn3@fe.edu.vn>
2022-04-08 16:42:18 +0700

    Lab: Randomize Words

--- ClassesAndObjects.Labs/Program.cs
+++ ClassesAndObjects.Labs/Program.cs
@@ -1,5 +1,7 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Globalization;
+using System.Linq;
 
 namespace ClassesAndObjects.Labs
 {
@@ -17,6 +19,21 @@ namespace ClassesAndObjects.Labs
       CultureInfo provider = new CultureInfo("fr-FR");
       DateTime date = DateTime.ParseExact(inputDate, format, provider);
       Console.WriteLine(date.DayOfWeek);
+
+      //Randomize Words
+      //You are given a list of words in one line.
+      //Randomize their order and print each word at a separate line.
+      string inputLine = Console.ReadLine();
+      List<string> listString = inputLine
+                                    .Split()
+                                    .ToList();
+      Random rnd = new Random();
+      while (listString.Count != 0)
+      {
+        int temp = rnd.Next(0, listString.Count - 1);
+        Console.WriteLine(listString[temp]);
+        listString.RemoveAt(temp);
+      }
 
 
     }
