794c3a264d939dc6c4de4e8c3b88f3c843801088
Vinh Hoang <vinhhn3@fe.edu.vn>
2022-04-08 16:07:59 +0700

    Lab: Day of Week

---
+++ ClassesAndObjects.Labs/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Globalization;
+
+namespace ClassesAndObjects.Labs
+{
+  internal class Program
+  {
+    static void Main(string[] args)
+    {
+      Console.WriteLine("Hello World!");
+      // Day of Week
+      // You are given a date in format day-month-year.
+      // Calculate and print the day of week in English.
+      Console.WriteLine("Enter a date: dd/MM/yyyy");
+      string inputDate = Console.ReadLine();
+      string format = "dd/MM/yyyy";
+      CultureInfo provider = new CultureInfo("fr-FR");
+      DateTime date = DateTime.ParseExact(inputDate, format, provider);
+      Console.WriteLine(date.DayOfWeek);
+
+
+    }
+  }
+}
