5b7bd2e3895ace16171d55fa7fe6b4db0af9916a
Vinh Hoang <vinhhn3@fe.edu.vn>
2022-04-08 13:47:59 +0700

    Constructors

--- ClassesAndObjects/Program.cs
+++ ClassesAndObjects/Program.cs
@@ -9,27 +9,67 @@ namespace ClassesAndObjects
     public string FullName { get; set; }
     public byte Age { get; set; }
     public string Address { get; set; }
+
+    // Constructor
+    // Without Parameter
+    public Person()
+    {
+      FullName = "Default Name";
+      Age = 0;
+      Address = "Default Address";
+    }
+
+    // With Params
+    public Person(string fullName, byte age, string address)
+    {
+      FullName = fullName;
+      Age = age;
+      Address = address;
+    }
+
+    public Person(string fullName, byte age)
+    {
+      FullName = fullName;
+      Age = age;
+      Address = "Homeless";
+    }
+
+    public Person(string firstName, string lastName, byte age)
+    {
+      FullName = firstName + " " + lastName;
+      Age = age;
+      Address = "No Way Home";
+    }
   }
 
   internal class Program
   {
     static void Main(string[] args)
     {
+      Person defaultPerson = new Person();
+      Person anotherPerson = new Person();
+
       // Declare objects Person
       // keyword: new
-      Person alice = new Person();
+      Person alice = new Person(
+        "Alice Kim", 35, "127B Agent Street");
       // 
       // Assigned values to properties of object
       // ".": access property of an object
-      // VariableName.PropertyName
-      alice.FullName = "ALice Kim";
-      alice.Age = 35;
-      alice.Address = "127B Agent Street";
+      //// VariableName.PropertyName
+      //alice.FullName = "ALice Kim";
+      //alice.Age = 35;
+      //alice.Address = "127B Agent Street";
 
-      Person john = new Person();
-      john.FullName = "John Wick";
-      john.Age = 45;
-      john.Address = "Hotel California";
+      Person john = new Person(
+        "John Wick", 45, "Hotel California"
+        );
+      //john.FullName = "John Wick";
+      //john.Age = 45;
+      //john.Address = "Hotel California";
+
+      Person spiderMan = new Person(
+        "Peter", "Parker", 23);
 
       // Print info
       Console.WriteLine($"Full Name: {alice.FullName}");
