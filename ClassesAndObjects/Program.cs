8c5aef9e20734582b75a16aad6757c853757fde4
Vinh Hoang <vinhhn3@fe.edu.vn>
2022-04-08 14:59:56 +0700

    Interaction between Objects

--- ClassesAndObjects/Program.cs
+++ ClassesAndObjects/Program.cs
@@ -2,58 +2,6 @@
 
 namespace ClassesAndObjects
 {
-  // Define class Person
-  class Person
-  {
-    // Properties
-    public string FullName { get; set; }
-    public byte Age { get; set; }
-    public string Address { get; set; }
-
-    // Constructor
-    // Without Parameter
-    public Person()
-    {
-      FullName = "Default Name";
-      Age = 0;
-      Address = "Default Address";
-    }
-
-    // With Params
-    public Person(string fullName, byte age, string address)
-    {
-      FullName = fullName;
-      Age = age;
-      Address = address;
-    }
-
-    public Person(string fullName, byte age)
-    {
-      FullName = fullName;
-      Age = age;
-      Address = "Homeless";
-    }
-
-    public Person(string firstName, string lastName, byte age)
-    {
-      FullName = firstName + " " + lastName;
-      Age = age;
-      Address = "No Way Home";
-    }
-
-    // Methods
-    public void Eat()
-    {
-      Console.WriteLine("Eating ...");
-    }
-
-    public void Eat(string food)
-    {
-      Console.WriteLine($"{FullName} is eating {food}");
-    }
-
-  }
-
   internal class Program
   {
     static void Main(string[] args)
@@ -90,6 +38,14 @@ namespace ClassesAndObjects
 
       spiderMan.Eat();
       spiderMan.Eat("Pizza");
+
+      alice.Talk(john);
+      alice.Talk(spiderMan);
+      spiderMan.Talk(john);
+
+      Telephone phone = new Telephone("ABC@1234", "Apple", "iphone 7");
+      spiderMan.Call(phone, "090111222");
     }
+
   }
 }
