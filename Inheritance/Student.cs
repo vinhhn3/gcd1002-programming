af3217d12f0ee46c6c3255bdaf1ff1f5510c04d5
Vinh Hoang <vinhhn3@fe.edu.vn>
2022-04-13 14:01:26 +0700

    Introduce base keyword

--- Inheritance/Student.cs
+++ Inheritance/Student.cs
@@ -3,5 +3,27 @@
   // Student inherits Person
   class Student : Person
   {
+    public string School { get; set; }
+    public string Major { get; set; }
+
+
+    // base keyword
+    // Call function of the Parent clas
+    public Student() : base()
+    {
+      //FullName = "Default Name";
+      //Age = 0;
+      //Address = "Default Address";
+      School = "Default School";
+      Major = "Default Major";
+    }
+
+    public Student(string fullName, byte age,
+      string address, string school, string major)
+      : base(fullName, age, address)
+    {
+      School = school;
+      Major = major;
+    }
   }
 }
