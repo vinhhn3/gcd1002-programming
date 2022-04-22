75d976503d251759dc69fff52a5cb17cde5265e2
Vinh Hoang <vinhhn3@fe.edu.vn>
2022-04-22 13:42:10 +0700

    Setup basic structure of the application

---
+++ BookStoreApplication/BookStore.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections.Generic;
+
+namespace BookStoreApplication
+{
+  class BookStore
+  {
+    public List<Book> Books { get; private set; }
+    public string Name { get; private set; }
+    private BookStore()
+    {
+
+    }
+    public BookStore(string name)
+    {
+      Name = name;
+      Books = new List<Book>();
+    }
+
+    public void AddBook(string name, string author, double price)
+    {
+      Book newBook = new Book(Books.Count + 1, name, author, price);
+      Books.Add(newBook);
+    }
+  }
+}
---
+++ BookStoreApplication/Program.cs
@@ -0,0 +1,21 @@
+﻿namespace BookStoreApplication
+{
+  internal class Program
+  {
+    static void Main(string[] args)
+    {
+      BookStore store = new BookStore("Avengers");
+      BookPresentator presentator = new BookPresentator();
+      Book book = new Book(1, "C# Basic 1", "Kent Beck", 123);
+      presentator.DisplayBookInfo(book);
+
+      store.AddBook("C# Basic 2", "Kent Beck", 123);
+      store.AddBook("C# Intermediate", "Kent Beck", 234);
+      store.AddBook("C# Advanced 1", "Kent Beck", 345);
+      store.AddBook("C# Advanced 2", "Kent Beck", 456);
+
+      presentator.DisplayBooksInStore(store);
+
+    }
+  }
+}
